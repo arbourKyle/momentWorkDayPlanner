165a33e091be4c6886318da6f547f9c2443fc079
KyleA <arbourkyle@gmail.com>
2022-01-28 19:16:41 -0500

    testing if statements and style changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
-var calendarArr = $(["#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17"]);
-var greyedOut = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-// var selected = [];
+var greyedOut = [9, 10, 11, 12, 13, 14, 15, 16, 17, 19]
+
+var selected = [];
 var now = moment();
+console.log(now);
 let smallest = Math.min.apply(null, greyedOut);
 let largest = Math.max.apply(null, greyedOut);
 console.log(smallest);
@@ -12,35 +13,67 @@ console.log(largest);
 var start = moment().startOf('date').add(9, 'hours');
 console.log(start);
 
-
 //Header displaying todays date
 $("#currentDay").text(now.format("ddd, MMMM Do"));
 
+//Highlighting past input cells
+//if 10 > 9
+if(10 > start) {
+  $("#9").css("background-color", "grey");
+}
 
-let whatTime;
-switch (moment()) {
-  case 0:
-    whatTime > start;
-    break;
-  case 1:
-    whatTime == moment();
-    break;
-  case 2:
-    whatTime < largest;
-    break;
-    console.log(whatTime);
+//if 11 > 9
+else if(11 > start) {
+  $("#10").css("background-color", "grey");
+}
+
+//if 12 > 9
+else if(12 > start) {
+  $("#11").css("background-color", "grey");
+}
+
+//if 13 > 9
+else if(13 > start) {
+  $("#12").css("background-color", "grey");
+}
+
+//if 14 > 9
+else if(14 > start) {
+  $("#13").css("background-color", "grey");
+}
+
+//if 15 > 9
+else if(15 > start) {
+  $("#14").css("background-color", "grey");
+}
+
+//if 16 > 9
+else if(16 > start) {
+  $("#15").css("background-color", "grey");
 }
 
 
-
-
-function myArrayMin(greyedOut) {
-if(now > start) {
-}
 else {
-  return
+  $(".form-control").css("background-color", "grey");
 }
+
+//8 cases
+//Highlighting current cell
+
+if(greyedOut.includes(now)){
+  [i].css("background-color", "red");
+  selected.push[i];
 }
+
+console.log(selected);
+
+
+
+
+
+
+
+
 
 
 
